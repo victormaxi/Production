6384926c67dd97b5b3679997a604bf561683a784
aadeyemi <aadeyemi@chams.com>
2020-01-28 15:02:35 +0100

    Commit Staged

---
+++ Production.Api.Core/Models/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Production.Api.Core.Models
+{
+   public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+    }
+}
